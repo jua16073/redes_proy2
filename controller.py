0416de65c6a19b40e55ea89b208fdcdbdbcbf90c
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-09-11 10:39:34 -0600

    verifica si un movimiento es valido

--- controller.py
+++ controller.py
@@ -14,6 +14,7 @@ class Controller():
         self.deck = Deck.Deck()
         self.players = []
         self.game_over = False
+        ## current_play: [("Two", 15), ("Three", 3)]
         self.current_play = []
 
     ## Players: list of players that is in the game
@@ -29,14 +30,34 @@ class Controller():
 
         ## Validates if the current play has more or equal cards that the player move
         if len(self.current_play)<= len(cards):
+
+            # More than 1 card
             if len(cards)>1:
-                ##Si se escogen varias cartas que no son iguales es una movida invalida 
+                ##If the layer has more tha 1 card, it looks if they are the same
                 if(len(set(cards))>1):
                     return False
-                
-                #### Validar si las cartas anteriores son mayores o iguales que ahorita 
-            
-            
+                else: 
+                    ## If the last played has the same or less cards that the current play
+                    if len(cards)>= len(self.current_play):
+                        ##if the played cards are the same or higher then it can be played
+                        current_card = self.current_play[0]
+                        played_card = cards[0]
+                        if(current_card[1]> played_card[1]):
+                            return False
+                        else: 
+                            return True
+
+        ## if the current play has more than the new play then its false
+        else: 
+            return False
+
+        #Compares the card 
+        current_card = self.current_play[0]
+        played_card = cards[0]
+        if(current_card[1]> played_card[1]):
+            return False
+        else: 
+            return True
 
             
 
