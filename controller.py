4e24bf63ab29e1c47f5d7c8a448b3012f46fc728
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-09-11 09:53:37 -0600

    changes

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@
 # controller.py: control the game    #
 
 import Deck
+import Player
 
 class Controller():
 
@@ -13,8 +14,30 @@ class Controller():
         self.deck = Deck.Deck()
         self.players = []
         self.game_over = False
+        self.current_play = []
 
     ## Players: list of players that is in the game
     def distribute_cards(self, player):
         return 0
         
+    def make_move(self):
+        return 0
+
+    ## cards: list of cards that the player selects to move
+    ## cards: [("Two", 15), ("Three", 3)]
+    def valid_move(self, cards):
+
+        ## Validates if the current play has more or equal cards that the player move
+        if len(self.current_play)<= len(cards):
+            if len(cards)>1:
+                ##Si se escogen varias cartas que no son iguales es una movida invalida 
+                if(len(set(cards))>1):
+                    return False
+                
+                #### Validar si las cartas anteriores son mayores o iguales que ahorita 
+            
+            
+
+            
+
+
--- player.py
+++ player.py
@@ -16,3 +16,18 @@ class Player():
 	def select_card(self, card):
 		self.cards_left.remove(card)
 		self.cards_selected.append(card)
+
+	## cards list of cards that the player have to remove
+	def remove_card(self, cards):
+		for card in cards:
+			self.cards_left.remove(card)
+
+	## cards: list of cards that the player has to add to his hand
+	def add_cards(self, cards):
+		self.cards_left.append(cards)
+
+	## card: card thats is going back to players hand because he made an invalid move
+	def invalid_card(self, card):
+		self.cards_left.append(card)
+		self.cards_selected.remove(card)
+
